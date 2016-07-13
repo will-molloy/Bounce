facc6b77b290ae842433eda4cdcf26066230477b
Will <willjoemolloy@gmail.com>
2016-07-13 20:43:02 +1200

    Cleaned Bounce/bounce-3/src/bounce/forms/ImageShapeFormHandler.java

--- bounce-3/src/bounce/forms/ImageShapeFormHandler.java
+++ bounce-3/src/bounce/forms/ImageShapeFormHandler.java
@@ -4,9 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import javax.imageio.ImageIO;
 import javax.swing.SwingWorker;
@@ -18,18 +16,18 @@ import bounce.forms.util.Form;
 import bounce.forms.util.FormHandler;
 
 /**
- * FormHandler for instantiating class ImageRectangleShape using
- * a SwingWorker to process and add the ImageRectangleShape on a 
+ * FormHandler for instantiating the class ImageRectangleShape using
+ * a SwingWorker to handle loading an image and scaling if nessesary on a
  * background thread.
  * 
- * @author Will
+ * @author Will Molloy
  */
 public class ImageShapeFormHandler<T> implements FormHandler {
 
 	private ShapeModel _model;
 	private NestingShape _nest;
-
-	private SwingWorker<List<Form>, Form> _worker;
+	private Form _imageForm;
+	private SwingWorker<BufferedImage, Void> _worker;
 
 	public ImageShapeFormHandler (ShapeModel model, NestingShape nest){
 		_model = model;
@@ -43,90 +41,106 @@ public class ImageShapeFormHandler<T> implements FormHandler {
 	 */
 	@Override
 	public void processForm(Form form) {
-		_worker = new ImageShapeWorker(
-				new ArrayList<Form>(Arrays.asList(form))
-				);
+		_imageForm = form;
+		_worker = new ImageShapeWorker();
 		_worker.execute(); 
 	}
 
 	/**
-	 * Worker class, instantiates ImageRectangleShape objects in the background process.
-	 * @author Will
-	 *
-	 */
-	private class ImageShapeWorker extends SwingWorker<List<Form>, Form> {
+	 * Worker class --
+	 * Loads and Scales (if needed) an Image on a background thread.
+	 * Then instantiates an ImageRectangleShape and adds it to the animation on
+	 * the ED thread.
+	 * 
+	 * @author Will Molloy
+	 */ 
+	private class ImageShapeWorker extends SwingWorker<BufferedImage, Void> {
 
-		List<Form> _forms;
+		private File _imageFile;
+		private int _width;
+		private int _deltaX;
+		private int _deltaY;
 
-		public ImageShapeWorker(List<Form> forms){
-			_forms = forms;
-		}
+		private int _originalImageWidth;
+		private int _originalImageHeight;
 
-		/**
-		 * Processes the RectangleImageShape forms on a background thread
-		 */
 		@Override
-		protected List<Form> doInBackground() throws Exception {
+		protected BufferedImage doInBackground() throws Exception {
+
+			BufferedImage originalImage, scaledImage;
 			
-			for (Form f : _forms){
-				try { 
-					processForm(f); 
-				} 
-				catch (Exception e) {}
+			getFieldValuesFromForm();
+
+			originalImage = loadOriginalImageFromImageFile(_imageFile);
+			getImageFieldValues(originalImage);
+
+			// Scale the image if necessary.
+			scaledImage = originalImage;
+			if (_originalImageWidth > _width){
+				scaledImage = scaleImage(originalImage);
 			}
-			return null;			
+			
+			return scaledImage;
 		}
-		
-		/**
-		 * Reads form data that describes an ImageRectangleShape. Based on the 
-		 * data, this SimpleImageShapeFormHandler creates a new ImageRectangleShape 
-		 * object, adds it to a ShapeModel and to a NestingShape within the model.
-		 * 
-		 * @param form the Form that contains the ImageRectangleShape data.
-		 */
-		private void processForm(Form form){
-			long startTime = System.currentTimeMillis();
 
-			// Read field values from the form.
-			File imageFile = (File)form.getFieldValue(File.class, ImageFormElement.IMAGE);
-			int width = form.getFieldValue(Integer.class, ShapeFormElement.WIDTH);
-			int deltaX = form.getFieldValue(Integer.class, ShapeFormElement.DELTA_X);
-			int deltaY = form.getFieldValue(Integer.class, ShapeFormElement.DELTA_Y);
+		private void getFieldValuesFromForm() {
+			_imageFile = (File)_imageForm.getFieldValue(File.class, ImageFormElement.IMAGE);
+			_width = _imageForm.getFieldValue(Integer.class, ShapeFormElement.WIDTH);
+			_deltaX = _imageForm.getFieldValue(Integer.class, ShapeFormElement.DELTA_X);
+			_deltaY = _imageForm.getFieldValue(Integer.class, ShapeFormElement.DELTA_Y);
+		}
 
-
-			// Load the original image (ImageIO.read() is a blocking call).
+		// Load the original image (ImageIO.read() is a blocking call).
+		private BufferedImage loadOriginalImageFromImageFile(File imageFile) {
 			BufferedImage fullImage = null;
 			try {
 				fullImage = ImageIO.read(imageFile);
 			} catch(IOException e) {
 				System.out.println("Error loading image.");
 			}
+			return fullImage;
+		}
 
-			int fullImageWidth = fullImage.getWidth();
-			int fullImageHeight = fullImage.getHeight();
+		private void getImageFieldValues(BufferedImage originalImage) {
+			_originalImageWidth = originalImage.getWidth();
+			_originalImageHeight = originalImage.getHeight();
+		}
 
-			BufferedImage scaledImage = fullImage;
+		private BufferedImage scaleImage(BufferedImage originalImage) {
+			long startTime = System.currentTimeMillis();
 
-			// Scale the image if necessary.
-			if(fullImageWidth > width) {
-				double scaleFactor = (double)width / (double)fullImageWidth;
-				int height = (int)((double)fullImageHeight * scaleFactor);
-
-				scaledImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB); 
-				Graphics2D g = scaledImage.createGraphics();
-
-				// Method drawImage() scales an already loaded image. The 
-				// ImageObserver argument is null because we don't need to monitor 
-				// the scaling operation.
-				g.drawImage(fullImage, 0, 0, width, height, null);
-			}
-
-			// Create the new Shape and add it to the model.
-			ImageRectangleShape imageShape = new ImageRectangleShape(deltaX, deltaY, scaledImage);
-			_model.add(imageShape, _nest);
+			BufferedImage scaledImage = originalImage;
 			
+			double scaleFactor = (double)_width / (double)_originalImageWidth;
+			int height = (int)((double)_originalImageHeight * scaleFactor);
+
+			scaledImage = new BufferedImage(_width,height,BufferedImage.TYPE_INT_RGB); 
+			Graphics2D g = scaledImage.createGraphics();
+
+			// Method drawImage() scales an already loaded image. The 
+			// ImageObserver argument is null because we don't need to monitor 
+			// the scaling operation.
+			g.drawImage(originalImage, 0, 0, _width, height, null);
+
 			long elapsedTime = System.currentTimeMillis() - startTime;
 			System.out.println("Image loading ans scaling took " + elapsedTime + "ms.");
-		}	
+
+			return scaledImage;
+		}
+
+		// Instantiate an ImageRectangleShape and add it to the animation -- on the ED thread
+		@Override
+		protected void done(){
+			try {
+				BufferedImage image = this.get();
+				// Create the new Shape and add it to the model.
+				ImageRectangleShape imageShape = new ImageRectangleShape(_deltaX, _deltaY, image);
+				_model.add(imageShape, _nest);
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
+
 }
